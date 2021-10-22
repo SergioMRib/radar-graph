a8b634a87774edfee1704aefd8e665a141cb07e3
Sérgio Ribeiro <sergio.m.rib@gmail.com>
2021-10-22 03:44:12 +0100

    Added redraw graphs

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,92 @@
 <script>
-	export let name;
+import {onMount} from "svelte";
+import { element } from "svelte/internal";
 
-	/* onMount(() => {
+
+	export let name;
+	let firstDraw = false;
+
+	onMount(() => {
         console.log("the component has mounted");
         Plotly.newPlot("myDiv", data, layout)
-    }); */
+    });
 
     // http://192.168.1.13:8001/?A1=66&A2=75&A3=18&A4=54&B1=54&B2=23&B3=68&B4=35&C1=62&C2=19&C3=79&C4=98&D1=30&D2=52&D3=86&D4=34&
 	function drawGraph() {
 
         Plotly.newPlot("myDiv", data, layout)
+		firstDraw = true;
 	}
 
+	function drawRandGraph() {
+
+		//Plotly.deleteTraces('myDiv', 0);
+		values = values.map(element => {
+			return Math.ceil(Math.random() * 50 + 50);
+		}); 
+		
+		let data = [
+      			{
+        			type: 'scatterpolar',
+        			r: values,
+        			theta: allAxis,
+        			fill: 'toself',
+        			name: 'Group A'
+      			},
+    	]
+
+    	let layout = {
+      		polar: {
+				radialaxis: {
+    	  		    visible: true,
+    	  		    range: [0, 100]
+    	        }
+    		}
+    	}
+		console.log("New values: " + data[0].r);
+		console.log("Drawing graph")
+        Plotly.newPlot("myDiv", data, layout)
+
+	}
+
+	function drawTwoGraphs() {
+
+		//Plotly.deleteTraces('myDiv', 0);
+		let values1 = values.map(element => {
+			return Math.ceil(Math.random() * 50 + 50);
+		}); 
+
+		let values2 = values.map(() => Math.ceil(Math.random() * 70 + 30));		
+		let data = [
+      			{
+        			type: 'scatterpolar',
+        			r: values1,
+        			theta: allAxis,
+        			fill: 'toself',
+        			name: 'Group A'
+      			},
+				  {
+					  type: 'scatterpolar',
+					  r: values2,
+					  theta: allAxis,
+					  fill: 'toself',
+					  name: 'Group B'
+				  }
+    	]
+
+    	let layout = {
+      		polar: {
+				radialaxis: {
+    	  		    visible: true,
+    	  		    range: [0, 100]
+    	        }
+    		}
+    	}
+		console.log("New values: " + data[0].r);
+		console.log("Drawing graph")
+        Plotly.newPlot("myDiv", data, layout)
+
+	}
     let allAxis = []
     let values = []
     let data = []
@@ -56,12 +131,15 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h3 class="mb-5">Your data results</h3>
 	<div>
-		<button on:click="{drawGraph}">Draw graph</button>
+		<button class="btn btn-primary" on:click="{drawGraph}">Redraw graph from query</button>
+		
+		
 	</div>
 	<div id="myDiv"></div>
-
+	<button class="btn btn-danger mr-2" on:click="{drawRandGraph}">Draw a random graph</button>
+	<button class="btn btn-warning ml-2" on:click="{drawTwoGraphs}">Draw two random graphs</button>
 
 </main>
 
@@ -73,10 +151,10 @@
 		margin: 0 auto;
 	}
 
-	h1 {
+	h3 {
 		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
+		font-size: 3em;
 		font-weight: 100;
 	}
 
